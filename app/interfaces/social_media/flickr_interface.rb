6db9758c588613024a189435e9d16087b43809e9
Guido Gloor <haslo@haslo.ch>
2015-12-30 21:19:58 +0100

    Flickr: fetch photos for user

--- app/interfaces/social_media/flickr_interface.rb
+++ app/interfaces/social_media/flickr_interface.rb
@@ -3,7 +3,9 @@ module SocialMedia
     class << self
 
       def index
-        # TODO
+        flickr_config = get_flickr_config
+        flickr = init_flickraw(flickr_config)
+        flickr.photos.search(:user_id => flickr_config['user_id'])
       end
 
       def create_from(image)
@@ -16,6 +18,19 @@ module SocialMedia
 
       def destroy_for(image)
         # TODO
+      end
+
+      private
+
+      # see https://github.com/hanklords/flickraw
+      def init_flickraw(flickr_config)
+        FlickRaw.api_key = flickr_config['api_key']
+        FlickRaw.shared_secret = flickr_config['shared_secret']
+        FlickRaw::Flickr.new
+      end
+
+      def get_flickr_config
+        YAML.load_file(Rails.root.join('config', 'accounts.yml'))[Rails.env]['flickr']
       end
 
     end
